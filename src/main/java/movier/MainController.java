d38deda063ffd54093d087b0e9499f7ba8d5bef3
Petronelll <41263719+Petronelll@users.noreply.github.com>
2022-01-13 13:22:34 +0200

    Add index page

--- src/main/java/movier/MainController.java
+++ src/main/java/movier/MainController.java
@@ -5,12 +5,9 @@ import movier.user.UserRepository;
 import movier.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class MainController {
 
     UserService userService;
